fbf768423bae2a898a0bc2d620031be28136a246
flaviogf <flavio.fernandes6@gmail.com>
2024-06-01 14:49:54 -0300

    feat: :sparkles: add sum function

---
+++ math_test.go
@@ -0,0 +1,23 @@
+package math
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	tableTest := []struct {
+		x        int
+		y        int
+		expected int
+	}{
+		{x: 10, y: 10, expected: 20},
+		{x: 20, y: 20, expected: 40},
+		{x: 30, y: 30, expected: 60},
+	}
+
+	for _, test := range tableTest {
+		got := Sum(test.x, test.y)
+
+		if test.expected != got {
+			t.Errorf("expected: %d, got: %d", test.expected, got)
+		}
+	}
+}
